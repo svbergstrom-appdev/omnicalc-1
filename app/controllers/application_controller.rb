21c6fc87e9cc799e12aba6e60ddbfed8351f67f3
svbergstrom <sbergstr@chicagobooth.edu>
2021-04-21 16:16:33 +0000

    random page done too

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
 class ApplicationController < ActionController::Base
   def blank_square_form
-
-
     render({ :template => "calculation_templates/square_form.html.erb"})
   end
 
@@ -9,6 +7,23 @@ class ApplicationController < ActionController::Base
     @num = params.fetch("number").to_f
     @square_of_num = @num ** 2
     render({ :template => "calculation_templates/square_results.html.erb"})
-    
   end
+
+  def blank_random_form
+    render({ :template => "calculation_templates/random_form.html.erb"})
+  end
+
+  def calculate_random
+    @lower = params.fetch("user_min").to_f
+    @upper = params.fetch("user_max").to_f
+    @result = rand(@lower..@upper)
+    render({ :template => "calculation_templates/rand_results.html.erb"})
+  end
+
+
+
+
+
+
+
 end
